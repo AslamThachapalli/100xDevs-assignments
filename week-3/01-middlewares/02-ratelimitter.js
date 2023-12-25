bd09c9d38d5ad54e9773860a291f86d069dfdd26
AslamThachapalli <aslam.develop912@gmail.com>
2023-12-25 18:13:49 +0530

    ratelimitter and errorcount solved

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,29 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+function ratelimitter(req, res, next) {
+  const userId = req.headers['user-id'];
+  
+  if(!userId){
+    res.sendStatus(404);
+    return;
+  }
+
+  if(numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId]++;
+    if(numberOfRequestsForUser[userId] > 5){
+      res.status(404).send("Request limit reached");
+    }else{
+      next();
+    }
+  }else{
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
+}
+
+app.use(ratelimitter);
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
