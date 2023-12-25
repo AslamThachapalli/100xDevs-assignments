20f12e1e468057b052ef3f256015f305c47e6882
Aslam Thachapalli <aslam@APPLEs-MacBook-Pro.local>
2023-12-25 23:23:12 +0530

    mongo-auth-admin-routes completed

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,26 @@
+const jwt = require('jsonwebtoken')
+
+const jwtPassword = 'secret123';
+
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
-    // Implement admin auth logic
-    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const authHeader = req.headers.authorization;
+    const tokenArray = authHeader.split(" ")
+    const token = tokenArray[1];
+
+    let isValid = true;
+
+    try {
+        jwt.verify(token, jwtPassword);
+    } catch (_) {
+        isValid = false;
+    }
+
+    if (isValid) { 
+        next();
+    }else{
+        res.sendStatus(401);
+    }
 }
 
 module.exports = adminMiddleware;
--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,22 +1,93 @@
 const { Router } = require("express");
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db/index")
 const router = Router();
 
+const jwtPassword = 'secret123';
+
+async function hashPassword(password) {
+    const hash = await bcrypt.hash(password, 10);
+    return hash;
+}
+
+async function comparePassword(password, hash) {
+    const result = await bcrypt.compare(password, hash);
+    return result;
+}
+
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post('/signup', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const hashedPass = await hashPassword(password);
+
+    const admin = new Admin({
+        username,
+        password: hashedPass,
+    })
+
+    admin.save().then(() => res.status(201).json({
+        message: "Admin created successfully"
+    }));
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
+router.post('/signin', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = await Admin.findOne({ username }).exec();
+
+    if (!user) {
+        res.status(404).send("user not found")
+        return;
+    }
+
+    const hashPass = user.password;
+    const result = await comparePassword(password, hashPass);
+
+    if (result) {
+        const token = jwt.sign({ username }, jwtPassword);
+
+        res.status(200).json({ token });
+    } else {
+        res.status(401).send("Wrong Credentials")
+    }
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post('/courses', adminMiddleware, async (req, res) => {
+    const course = req.body;
+    const id = Math.floor(Math.random() * 100000);
+
+    course["id"] = id;
+
+    try {
+        const newCourse = new Course(course);
+
+        await newCourse.save();
+        res.status(201).json({ message: 'Course created successfully', courseId: id })
+    } catch (e) {
+        if (e.name === "ValidationError") {
+            let errors = {};
+
+            Object.keys(e.errors).forEach((key) => {
+                errors[key] = e.errors[key].message;
+            });
+
+            return res.status(400).send(errors);
+        }
+        res.status(500).send("Something went wrong");
+    }
+
+
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get('/courses', adminMiddleware, async (req, res) => {
+    const allCourses = await Course.find({},{_id:0, __v:0});
+
+    res.status(400).json({courses: allCourses});
 });
 
 module.exports = router;
